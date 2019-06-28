9f5d9679c1a20797fd5801b25e8b98fd24bde529
zzj <zhangzjzzj@foxmail.com>
2019-06-28 20:28:53 +0800

    20190628

--- src/com/itcast/io/FileDemo.java
+++ src/com/itcast/io/FileDemo.java
@@ -1,31 +1,30 @@
 package com.itcast.io;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class FileDemo {
 
 	public static void main(String[] args) throws IOException {
-		File file = new File("a");
-		String[] list = file.list();
-		for (int i = 0; i < list.length; i++) {
-			System.out.println(list[i]);
+		FileReader fr = null;
+		try {
+			fr = new FileReader(new File("a\\a.txt"));
+			char[] buf = new char[4];
+			int len;
+			while((len = fr.read(buf)) != -1) {
+				System.out.println(new String(buf, 0, len));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (fr != null) {
+				try {
+					fr.close();
+				} catch (IOException e2) {
+					e2.printStackTrace();
+				}
+			}
 		}
-		
-//		File file = new File("a\\b");
-//		file.mkdir();
-//		file.delete();
-//		File file2 = new File("b");
-//		file2.delete();
-////		file2.createNewFile();
-//		System.out.println(file.exists());
-//		System.out.println(file.isDirectory());
-//		System.out.println(file.isFile());
-//		System.out.println(file.getAbsolutePath());
-//		System.out.println(file.getPath());
-//		System.out.println(file.getParent());
-//		System.out.println(file.getParentFile());
-//		System.out.println(file.getName());
-//		System.out.println(file.length());
 	}
 }
